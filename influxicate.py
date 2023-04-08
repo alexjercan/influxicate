253353d41ace6b27e7dccec2f5353756a0f70c85
Alex Jercan <jercan_alex27@yahoo.com>
2023-04-08 11:15:48 +0300

    like a chad

--- influxicate.py
+++ influxicate.py
@@ -1,19 +1,25 @@
 import argparse
+import json
 import logging
 import os
 from dataclasses import dataclass
+from pathlib import Path
+from typing import Dict, Optional
 
 import influxdb_client
+import pandas as pd
 
 
 @dataclass
 class Args:
     url: str
     org: str
+    bucket: str
     token: str
+    sensors: str
 
     def __repr__(self) -> str:
-        return f"Args(url='{self.url}', org='{self.org}')"
+        return f"Args(url='{self.url}', org='{self.org}'), sensors='{self.sensors}'"
 
 
 def parse_args():
@@ -34,11 +40,25 @@ def parse_args():
         help="provide the org name of the influxdb database",
     )
     parser.add_argument(
+        "-b",
+        "--bucket",
+        type=str,
+        default="my-bucket",
+        help="provide the bucket name of the influxdb database",
+    )
+    parser.add_argument(
         "-t",
         "--token",
         type=str,
         required=True,
         help="provide the token of the influxdb database",
+    )
+    parser.add_argument(
+        "-s",
+        "--sensors",
+        type=str,
+        required=True,
+        help="provide the path to the jsonl file that contains all csv files",
     )
 
     args = parser.parse_args()
@@ -46,8 +66,60 @@ def parse_args():
     return Args(
         url=args.url,
         org=args.org,
+        bucket=args.bucket,
         token=args.token,
+        sensors=args.sensors,
     )
+
+
+@dataclass
+class Sensor:
+    path: str
+    name: str
+    tags: Dict[str, str]
+
+
+def parse_sensor(
+    root_path: str,
+    path: str,
+    name: str,
+    tags: Optional[Dict[str, str]] = None,
+) -> Sensor:
+    return Sensor(
+        path=os.path.join(root_path, path),
+        name=name,
+        tags=tags or dict(),
+    )
+
+
+def write_sensor(
+    client: influxdb_client.InfluxDBClient,
+    bucket: str,
+    sensor: Sensor,
+):
+    COL_TIME = "time"
+
+    df = pd.read_csv(sensor.path)
+    names = list(df.columns)
+
+    assert (
+        COL_TIME in names
+    ), f"The {COL_TIME} column is required to be present in the csv file"
+
+    df = df.set_index(COL_TIME)
+
+    for tag, tag_value in sensor.tags.items():
+        df[tag] = tag_value
+
+    tag_columns = list(sensor.tags.keys())
+
+    with client.write_api() as write_api:
+        write_api.write(
+            bucket,
+            record=df,
+            data_frame_measurement_name=sensor.name,
+            data_frame_tag_columns=tag_columns,
+        )
 
 
 def main(args: Args):
@@ -59,7 +131,13 @@ def main(args: Args):
         org=args.org,
     )
 
-    print(client.health())
+    sensors_path = Path(args.sensors)
+    root_path = sensors_path.parent
+    with open(sensors_path, "r", encoding="utf-8") as f:
+        sensors = [parse_sensor(root_path, **json.loads(line)) for line in f]
+
+    for sensor in sensors:
+        write_sensor(client, args.bucket, sensor)
 
 
 if __name__ == "__main__":
